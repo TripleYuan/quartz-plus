9b9c9b851a5e889bf008a6a718eb091e3ac22c54
redcoder54 <redcoder54@gmail.com>
2022-06-21 15:47:20 +0800

    支持自定义Quartz JobKey name 和 TriggerKey name生成策略

--- quartz-extend-core/src/main/java/redcoder/quartzextendcore/core/QuartzJobProcessorSupport.java
+++ quartz-extend-core/src/main/java/redcoder/quartzextendcore/core/QuartzJobProcessorSupport.java
@@ -1,5 +1,6 @@
 package redcoder.quartzextendcore.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.springframework.util.StringUtils;
 import redcoder.quartzextendcore.annotation.QuartzJob;
@@ -9,16 +10,17 @@ import redcoder.quartzextendcore.annotation.QuartzTrigger;
  * @author wxy
  * @since 2022-06-21
  */
+@Slf4j
 public class QuartzJobProcessorSupport {
 
-    private QuartzKeyNameGenerator generator = new DefaultQuartzKeyNameGenerator();
+    private final QuartzKeyNameGenerator defaultQuartzKeyNameGenerator = new DefaultQuartzKeyNameGenerator();
 
     public String createJobKeyName(QuartzJob quartzJob, Class<? extends Job> jobClass) {
         String name = quartzJob.jobKeyName();
         if (StringUtils.hasText(name)) {
             return name;
         }
-        return generator.generateJobKeyName(jobClass);
+        return createGenerator(quartzJob.quartzKeyNameGenerator()).generateJobKeyName(jobClass);
     }
 
     public String createTriggerKeyName(QuartzTrigger quartzTrigger, Class<? extends Job> jobClass) {
@@ -26,6 +28,15 @@ public class QuartzJobProcessorSupport {
         if (StringUtils.hasText(name)) {
             return name;
         }
-        return generator.generateTriggerKeyName(jobClass);
+        return createGenerator(quartzTrigger.quartzKeyNameGenerator()).generateTriggerKeyName(jobClass);
+    }
+
+    private QuartzKeyNameGenerator createGenerator(Class<? extends QuartzKeyNameGenerator> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (Exception e) {
+            log.error("Failed to create QuartzKeyNameGenerator, we will use DefaultQuartzKeyNameGenerator.", e);
+            return defaultQuartzKeyNameGenerator;
+        }
     }
 }
